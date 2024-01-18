6c688525e8a2488e571fa326cceeee8e65ea302e
박인호 <i0n4h0o7@gmail.com>
2024-01-19 00:34:14 +0900

    Initial Commit

---
+++ voiceMemo/voiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceMemoApp.swift
+//  voiceMemo
+//
+//  Created by 박인호 on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct voiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
